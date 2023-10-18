b4a2f4fb9b0181399d74e14c43dafb609c3935b7
joefas <ochienggeofrey06@gmail.com>
2023-10-18 04:26:36 -0700

    updated printf

--- main.h
+++ main.h
@@ -5,18 +5,61 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <limits.h>
+#include <stdlib.h>
 
-int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
+#define UNUSED(x)(void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct format - struct op
+ *
+ * @fmt: The format
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int(*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - struct op
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ *
+ */
+
+typedef struct fmt fmt_t;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
+int handle_print(const char *fmt, int *i,
+		va_list list, char buffer[], int flags, int width, int precision, int size);
 
-#endif
+/* Function to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/* Function to print memory address */
+int print_pointer(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+
+/* Function to non printable characters */
+int print_non_printable(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+
+#endif/* MAIN_H */
